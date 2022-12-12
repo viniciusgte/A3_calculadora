fd17d6feaa0b050b7f494784929e5e944af59372
Vinícius Tachetti <114702851+viniciusgte@users.noreply.github.com>
2022-12-12 17:57:43 -0300

    inicio  sql exception

--- src/AppOrcamento.java
+++ src/AppOrcamento.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 public class AppOrcamento {
     public static double hd = 0;
     public static void main(String[] args) {
+        
         ItemSistema itemSistema = new ItemSistema();
         Profissional calculadora = new Profissional();
         Scanner sc = new Scanner(System.in);
+        
+        // Input das Informações Necessárias
         System.out.println("Digite seu nome abaixo: ");
         calculadora.Nome=sc.nextLine();
         System.out.println("Digite abaixo sua pretensão salarial: ");
@@ -20,12 +23,12 @@ public class AppOrcamento {
         System.out.print("Digite ao lado a complexidade de 1 a 3 da tela de gravação (Caso ela não exista, digite 0 para continuar): ");
         itemSistema.complex_Gravacao = sc.nextInt();
    
-    //    Formatação com padrão de moeda em Real
+        // Formatação com padrão de moeda em Real
         DecimalFormat money = new DecimalFormat("R$###,###,###,##0.00");
-    //    Formatação das horas
+        // Formatação das horas
         DecimalFormat time = new DecimalFormat("######0 horas");
 
-// Dados do Profissional
+        // Dados do Profissional
         System.out.println(" ");
         System.out.println(" ");
         System.out.println("ORÇAMENTO: ");
@@ -35,28 +38,31 @@ public class AppOrcamento {
         System.out.println("Valor/Hora: " + money.format(calculadora.CalculaValorHora()));
         System.out.println(" ");
     
-
+        // Lista de itens do sistema 
         ItemSistema[] lista_Itens_Sistemas = new ItemSistema[3];
 
+        // Cálculo Item 1 
         ItemSistema tela1 = new ItemSistema();
         tela1.nomeItem = "Tela E/S de dados";
         tela1.tipoItem = "Entrada e saida de dados";
         tela1.complex_EntradaSaida = itemSistema.complex_EntradaSaida;
         lista_Itens_Sistemas[0] = tela1;
-
+       
+        // Cálculo Item 2
         ItemSistema tela2 = new ItemSistema();
         tela2.nomeItem = "Tela de consulta com visualização de dados";
         tela2.tipoItem = "Consulta de dados";
         tela2.complex_Consulta = itemSistema.complex_Consulta;
         lista_Itens_Sistemas[1] = tela2;
 
+        // Cálculo Item 3
         ItemSistema tela3 = new ItemSistema();
         tela3.nomeItem = "Tela de cadastro com gravação de dados";
         tela3.tipoItem = "Gravação de dados";
         tela3.complex_Gravacao = itemSistema.complex_Gravacao;
         lista_Itens_Sistemas[2] = tela3;
 
-// Item 1
+        // Impressão Item 1
         System.out.println("------------------------------------");
         System.out.println(" ");
         System.out.println("Item 1 do orçamento: " + tela1.nomeItem);
@@ -67,7 +73,8 @@ public class AppOrcamento {
         System.out.println(" ");
         System.out.println("Quantidade de Horas Previstas: " + time.format(tela1.CalcularHorasEntradaSaida()));
         System.out.println(" ");
-// Item 2 
+
+        // Impressão Item 2 
         System.out.println("------------------------------------");
         System.out.println(" ");
         System.out.println("Item 2 do orçamento: " + tela2.nomeItem);
@@ -78,7 +85,8 @@ public class AppOrcamento {
         System.out.println(" ");
         System.out.println("Quantidade de Horas Previstas: " + time.format(tela2.CalcularHorasConsulta()));
         System.out.println(" ");
-// Item 3
+
+        // Impressão Item 3
         System.out.println("------------------------------------");
         System.out.println(" ");
         System.out.println("Item 3 do orçamento: " + tela3.nomeItem);
@@ -91,28 +99,26 @@ public class AppOrcamento {
         System.out.println(" ");
         System.out.println("------------------------------------");
 
+        // Tempo de construção do sistema
         int tempoTotalTelas = tela1.CalcularHorasEntradaSaida()+tela2.CalcularHorasConsulta()+tela3.CalcularHorasGravacao();
-        
-
         System.out.println(" ");
-        System.out.println("Horas totais necessárias para conclusão do sistema: Cerca de " + time.format(tempoTotalTelas));
+        System.out.println("Prazo para conclusão do sistema: Cerca de " + time.format(tempoTotalTelas));
         
-
+        // Valor sem adição dos valores de gestão, encargos e risco
         double valor_Sistema = calculadora.CalculaValorHora() * tempoTotalTelas;
-        // System.out.println("Valor puro: " + valor_Sistema);
-        
+                
         // Valor Sistema + 30% de gestão, encargos e risco.
         double valor_Total_Sistema = valor_Sistema + valor_Sistema*30/100; 
 
-        
-        System.out.println("\nValor/Hora do Orçamento: " + money.format(calculadora.CalculaValorHora()));
-        System.out.println("\nValor final do Orçamento: " + money.format(valor_Total_Sistema));
-        System.out.println(" ");
+        // Impressão final do orçamento
+        System.out.println("\n Valor/Hora do Orçamento: " + money.format(calculadora.CalculaValorHora()));
+        System.out.println("\n Valor final do Orçamento: " + money.format(valor_Total_Sistema));
         System.out.println(" ");
         System.out.println("Orçamento Concluído com Sucesso!");
         System.out.println(" ");
+        System.out.println("------------------------------------");
    
-   sc.close();
+    sc.close();
 
     }
 }
---
+++ src/Conexao.java
@@ -0,0 +1,8 @@
+// public class Conexao{
+    // try {
+        // Class.forName("com.mysql.jdbc.Driver");
+    // } catch (ClassNotFoundException ex) {
+        // System.out.print("Driver mySQL não localizado, ");
+        // TODO: handle exception
+    // }
+// }
